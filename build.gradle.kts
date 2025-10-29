43564c32a3512fe7e47f503a6b732cce77a41381
Leonid Shikhalkin <rikser123@yandex.ru>
2025-10-29 15:44:33 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+import org.gradle.api.publish.PublishingExtension
+
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.5"
+	id("io.spring.dependency-management") version "1.1.7"
+	`maven-publish`
+}
+
+configure<PublishingExtension> {
+	publications {
+		register<MavenPublication>("gpr") {
+			from(components["java"])
+		}
+	}
+	repositories {
+		maven {
+			name = "GitHubPackages"
+			url = uri("https://maven.pkg.github.com/rikser123/rikser-bundlegit")
+			credentials {
+				username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+				password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+			}
+		}
+	}
+}
+
+group = "rikser123"
+version = "0.0.1"
+description = "Bundle"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+val mockitoAgent = configurations.create("mockitoAgent")
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation:3.5.6")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	implementation("org.liquibase:liquibase-core")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
+	implementation("io.jsonwebtoken:jjwt:0.13.0")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	mockitoAgent("org.mockito:mockito-core") { isTransitive = false }
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	testCompileOnly("org.projectlombok:lombok:1.18.30")
+	testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+	jvmArgs("-javaagent:${mockitoAgent.asPath}")
+}
+
+
---
+++ src/test/java/rikser123/bundle/validation/IsEqualConstraintTest.java
@@ -0,0 +1,85 @@
+package rikser123.bundle.validation;
+
+
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.Data;
+import org.hibernate.validator.internal.util.annotation.ConstraintAnnotationDescriptor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+public class IsEqualConstraintTest {
+    private ConstraintValidatorContext context;
+    private ConstraintValidatorContext.ConstraintViolationBuilder builder;
+    private ConstraintValidatorContext.ConstraintViolationBuilder.NodeBuilderCustomizableContext nodeContext;
+    private IsEqual annotation;
+
+    @BeforeEach()
+    void init() {
+        context = Mockito.mock(ConstraintValidatorContext.class);
+        builder = Mockito.mock(ConstraintValidatorContext.ConstraintViolationBuilder.class);
+        nodeContext = Mockito.mock(ConstraintValidatorContext.ConstraintViolationBuilder.NodeBuilderCustomizableContext.class);
+        annotation = getIsEqualAnnotation("password", "passwordConfirmation");
+
+        when(context.buildConstraintViolationWithTemplate(any())).thenReturn(builder);
+        when(builder.addPropertyNode(any())).thenReturn(nodeContext);
+    }
+
+    @Test
+    void shouldDetectEqual() {
+        var constraint = new IsEqualConstraint();
+        var userRequestDto = new CreateUserRequestDto();
+        userRequestDto.setPassword("a");
+        userRequestDto.setPasswordConfirmation("a");
+
+        constraint.initialize(annotation);
+        var result = constraint.isValid(userRequestDto, context);
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldDetectNotEqual() {
+        var constraint = new IsEqualConstraint();
+        var userRequestDto = new CreateUserRequestDto();
+        userRequestDto.setPassword("ab");
+        userRequestDto.setPasswordConfirmation("a");
+
+        constraint.initialize(annotation);
+        var result = constraint.isValid(userRequestDto, context);
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldDetectIfNoFields() {
+        var constraint = new IsEqualConstraint();
+        var userRequestDto = new CreateUserRequestDto();
+        userRequestDto.setPassword("ab");
+        userRequestDto.setPasswordConfirmation("a");
+
+        annotation = getIsEqualAnnotation("field1", "field2");
+
+        constraint.initialize(annotation);
+        var result = constraint.isValid(userRequestDto, context);
+        assertFalse(result);
+    }
+
+    private IsEqual getIsEqualAnnotation(String firstFieldValue, String secondFieldValue) {
+        var annotation = new ConstraintAnnotationDescriptor.Builder<>(IsEqual.class);
+        annotation.setAttribute("firstField", firstFieldValue);
+        annotation.setAttribute("secondField", secondFieldValue);
+
+        return annotation.build().getAnnotation();
+
+    }
+
+    @Data
+    private static class CreateUserRequestDto {
+        private String password;
+        private String passwordConfirmation;
+    }
+}
