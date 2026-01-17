a246de271069b06cc4b6b47ab20b11cb95e57b75
Leonid Shikhalkin <rikser123@yandex.ru>
2026-01-17 23:07:48 +0300

    auto config starter

---
+++ src/main/java/rikser123/bundle/config/BundleAutoConfig.java
@@ -0,0 +1,13 @@
+package rikser123.bundle.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Автоконфигурация бандла
+ */
+
+@Configuration
+@ComponentScan("rikser123.bundle")
+public class BundleAutoConfig {
+}
--- src/main/java/rikser123/bundle/config/BundleStarter.java
+++
@@ -1,7 +0,0 @@
-package rikser123.bundle.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class BundleStarter {
-}
