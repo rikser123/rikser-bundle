5b34728a7aa669f7671a21b3eae67246b916d9ca
Leonid Shikhalkin <rikser123@yandex.ru>
2026-02-06 19:23:55 +0300

    jackson customizer

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ configure<PublishingExtension> {
 }
 
 group = "rikser123"
-version = "0.0.23"
+version = "0.0.24"
 description = "Bundle"
 
 java {
--- src/main/java/rikser123/bundle/autoconfigure/BundleAutoConfiguration.java
+++ src/main/java/rikser123/bundle/autoconfigure/BundleAutoConfiguration.java
@@ -2,13 +2,12 @@ package rikser123.bundle.autoconfigure;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import rikser123.bundle.advice.GlobalExceptionHandler;
 import rikser123.bundle.component.TransactionHandler;
 import rikser123.bundle.service.StatusMatrix;
@@ -37,19 +36,30 @@ public class BundleAutoConfiguration {
     }
 
     @Bean
-    @Primary
-    public ObjectMapper customObjectMapper() {
-        return new ObjectMapper()
-            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
-            .configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
-            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-            .findAndRegisterModules()
-            .registerModule(new ParameterNamesModule())
-            .registerModule(new JavaTimeModule())
-            .setDefaultPropertyInclusion(
-                JsonInclude.Value.construct(JsonInclude.Include.NON_NULL, JsonInclude.Include.NON_NULL))
-            .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
+        return builder -> {
+            builder.featuresToDisable(
+                    SerializationFeature.FAIL_ON_EMPTY_BEANS,
+                    SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
+                    SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS
+            );
+
+            builder.featuresToEnable(
+                    DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
+            );
+
+            builder.postConfigurer(objectMapper -> {
+                objectMapper.configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+            });
+
+            builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+
+            builder.modulesToInstall(
+                    new ParameterNamesModule(),
+                    new JavaTimeModule()
+            );
+
+            builder.findModulesViaServiceLoader(true);
+        };
     }
 }
