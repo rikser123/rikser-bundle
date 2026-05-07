e8e864a8316464fe59e62e40def7c3e9522e70a3
Leonid Shikhalkin <rikser123@yandex.ru>
2026-05-07 19:25:21 +0300

    logging

--- src/main/java/rikser123/bundle/feign/SecurityClient.java
+++ src/main/java/rikser123/bundle/feign/SecurityClient.java
@@ -1,11 +1,11 @@
 package rikser123.bundle.feign;
 
 import org.springframework.http.MediaType;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import reactivefeign.spring.config.ReactiveFeignClient;
 import reactor.core.publisher.Mono;
+import rikser123.bundle.dto.User;
 import rikser123.bundle.dto.response.RikserResponseItem;
 
 @ReactiveFeignClient(
@@ -18,7 +18,7 @@ public interface SecurityClient {
     produces = MediaType.APPLICATION_JSON_VALUE,
     consumes = MediaType.APPLICATION_JSON_VALUE
   )
-  Mono<RikserResponseItem<UserDetails>> getUser(
+  Mono<RikserResponseItem<User>> getUser(
     @RequestHeader("Authorization") String authorization
   );
 }
--- src/main/java/rikser123/bundle/service/impl/UserDetailServiceImpl.java
+++ src/main/java/rikser123/bundle/service/impl/UserDetailServiceImpl.java
@@ -2,13 +2,13 @@ package rikser123.bundle.service.impl;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-import rikser123.bundle.dto.response.RikserResponseItem;
 import rikser123.bundle.feign.SecurityClient;
 import rikser123.bundle.service.UserDetailService;
 
@@ -18,6 +18,7 @@ import rikser123.bundle.service.UserDetailService;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserDetailServiceImpl implements UserDetailService {
   public static final String BEARER_PREFIX = "Bearer ";
   private final SecurityClient securityClient;
@@ -37,7 +38,13 @@ public class UserDetailServiceImpl implements UserDetailService {
   @Override
   public Mono<UserDetails> getByUsername(String token) {
     return securityClient.getUser(BEARER_PREFIX + token)
-      .map(RikserResponseItem::getData)
+      .doOnNext(sys -> {
+        log.warn("sys {}", sys);
+      })
+      .map(response -> {
+        log.warn("user {}", response);
+        return (UserDetails) response.getData();
+      })
       .onErrorResume(e -> Mono.error(new EntityNotFoundException("Пользователь не найден")));
   }
 }
