4eab6ab40834872f64ed3b0544eaaa1f7c63d834
Leonid Shikhalkin <rikser123@yandex.ru>
2026-05-10 20:02:51 +0300

    remove reactive feign cliebt

--- src/main/java/rikser123/bundle/feign/SecurityClient.java
+++ src/main/java/rikser123/bundle/feign/SecurityClient.java
@@ -3,15 +3,10 @@ package rikser123.bundle.feign;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import reactivefeign.spring.config.ReactiveFeignClient;
 import reactor.core.publisher.Mono;
 import rikser123.bundle.dto.User;
 import rikser123.bundle.dto.response.RikserResponseItem;
 
-@ReactiveFeignClient(
-  url = "${security.service.url}",
-  name = "security-client"
-)
 public interface SecurityClient {
   @GetMapping(
     value = "/api/v1/user/token",
