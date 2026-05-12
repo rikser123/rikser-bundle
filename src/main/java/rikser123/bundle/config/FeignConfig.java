04d077d583497591f9393c2c0bde64684e9e5b58
Leonid Shikhalkin <rikser123@yandex.ru>
2026-05-12 09:13:40 +0300

    logging

--- src/main/java/rikser123/bundle/config/FeignConfig.java
+++ src/main/java/rikser123/bundle/config/FeignConfig.java
@@ -1,18 +1,21 @@
 package rikser123.bundle.config;
 
 import feign.RequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
+@Slf4j
 public class FeignConfig {
 
   @Bean
   public RequestInterceptor authorizationHeaderInterceptor() {
     return template -> {
       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      log.warn("auth", authentication);
 
       if (authentication != null && authentication.getDetails() instanceof String token) {
         template.header("Authorization", "Bearer " + token);
