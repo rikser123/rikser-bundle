5fe18771795846fb23dcd08f3ed2923c08578c3b
Leonid Shikhalkin <rikser123@yandex.ru>
2026-05-19 11:39:06 +0300

    add ignore properties

--- src/main/java/rikser123/bundle/dto/User.java
+++ src/main/java/rikser123/bundle/dto/User.java
@@ -1,5 +1,6 @@
 package rikser123.bundle.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User implements UserDetails {
   private UUID id;
   private String login;
