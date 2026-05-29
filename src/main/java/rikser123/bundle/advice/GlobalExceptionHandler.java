33a8be03e2fc7bc487dcd9c060e4156db58eb544
Leonid Shikhalkin <rikser123@yandex.ru>
2026-05-29 20:09:34 +0300

    response interceptor

--- src/main/java/rikser123/bundle/advice/GlobalExceptionHandler.java
+++ src/main/java/rikser123/bundle/advice/GlobalExceptionHandler.java
@@ -21,9 +21,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-/**
- * Класс для работы с глобальными исключениями (синхронная версия)
- */
 
 @RestControllerAdvice
 @Slf4j
--- src/main/java/rikser123/bundle/config/ComponentConfig.java
+++ src/main/java/rikser123/bundle/config/ComponentConfig.java
@@ -3,12 +3,18 @@ package rikser123.bundle.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import rikser123.bundle.advice.GlobalExceptionHandler;
+import rikser123.bundle.advice.RikserResponseInterceptor;
 import rikser123.bundle.component.TransactionHandler;
 import rikser123.bundle.service.StatusMatrix;
 import rikser123.bundle.service.impl.StatusMatrixImpl;
 
 @Configuration
 public class ComponentConfig {
+  @Bean
+  public RikserResponseInterceptor rikserResponseInterceptor() {
+    return new RikserResponseInterceptor();
+  }
+
   @Bean
   public GlobalExceptionHandler globalExceptionHandler() {
     return new GlobalExceptionHandler();
